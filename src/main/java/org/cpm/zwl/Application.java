e6cfb4dd987f1c0c90310e02526a91b197963a99
DESKTOP-625Q0C6\Mailu <cpm.chuck@gmail.com>
2018-03-08 22:07:06 +0800

    add zuul config

--- src/main/java/org/cpm/zwl/Application.java
+++ src/main/java/org/cpm/zwl/Application.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 @EnableDiscoveryClient
-public class App {
-	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
-	}
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 }
